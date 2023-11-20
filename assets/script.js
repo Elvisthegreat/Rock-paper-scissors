cb31e78bd4e932411b8f952dfc995735206a3338
Elvisthegreat <>
2023-11-20 15:10:51 +0000

    Add if statement to check if player is equal to computer

--- assets/script.js
+++ assets/script.js
@@ -11,12 +11,32 @@ choiceBtns.forEach(button => button.addEventListener("click", () => {
 
     player = button.textContent;
     computerTurn();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    resultText.textContent = checkWinner();
 }));
 
 function computerTurn(){
     const randNumb = Math.floor(Math.random() * 3) + 1;
 
     switch (randNumb){
-        
+       case 1:
+        computer = "ROCK";
+        break;
+        case 2:
+            computer = "PAPER";
+            break; 
+            case 3:
+                computer = "SCISSORS";
+                break;
     }
+}
+
+function checkWinner(){
+    if(player == computer){
+    return "Draw!";
+
+   }else if (computer == "Rock" ){
+    return (player == paper)
+   }
 }
