f457372edd21da9333022356a5b85d52ef87a6a9
Elvisthegreat <>
2023-11-20 08:20:41 +0000

    Add some javascript functions

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,22 @@
-var playerText =document.querySelector("playerText");
-var computerText = document.querySelector("computerText");
-var resultText = document.querySelector("resultText");
+var playerText =document.querySelector("#playerText");
+var computerText = document.querySelector("#computerText");
+var resultText = document.querySelector("#resultText");
+var choiceBtns = document.querySelectorAll(".choiceBtn");
+
+let player;
+let computer;
+let result;
+
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+
+    player = button.textContent;
+    computerTurn();
+}));
+
+function computerTurn(){
+    const randNumb = Math.floor(Math.random() * 3) + 1;
+
+    switch (randNumb){
+        
+    }
+}
